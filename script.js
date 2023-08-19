6c292c460373b6950aa80c938bfdced7eae2d75d
Abdirahman Jama <mustalid21@gmail.com>
2023-08-19 12:50:54 -0500

    added totalMontly cost section

--- script.js
+++ script.js
@@ -17,24 +17,12 @@ function addEmployeeInfo(event){
     let submisionButton = $('#submitButton').val();
     // console.log('add employee button works');
     // console.log(tableRow)
+    let monthlyCost = annualSalary / 12;
+
     let tableRow = `<tr> <td>${firstName}</td><td>${lastName}</td><td>${idTextInput}</td><td>${titleInput}</td><td>${annualSalary}</td></tr></tbody>`
     // console.log(tableRow)
     $('#insertionPoint').append(tableRow);
+
+
+
 }
-
-
-
-// X- A 'Submit' button should collect the form information------DONE
-// X -store the information append information to the DOM -------DONE
-// calculate monthly costs, -------------------------------------TODO
-
-// clear the input fields. ---------------------------------------TODO
-// Using the stored information, calculate monthly costs ---------TODO
-// append this to the to DOM. ------------------------------------TODO
-
-// If the total monthly cost exceeds $20,000, 
-// add a red background color to the total monthly cost.-----------TODO
-
-// Create a delete button that removes an employee from the DOM.---TODO
-// For Base mode, it does **not** need to remove that Employee's salary 
-//from the reported total.-----------------------------------------TODO
