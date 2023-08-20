c25c4046a6e640d060359e2d9dda8813595f7bb7
Abdirahman Jama <mustalid21@gmail.com>
2023-08-20 15:05:52 -0500

    modified html classes and added new IDs to use for removing data

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 $(document).ready(onReady);
 // console.log('js works!!!');
-
+let salaries =[];
 function onReady(){
+    // console.log('jquery is working!!!');
     $('#submitButton').on('click', addEmployeeInfo)
-
- 
 }
 
-function addEmployeeInfo(event){
+
+//this function will be used to submit new employees info to the table. 
+function addEmployeeInfo(event) {
     event.preventDefault();
+// storing employee data from input using .val. 
     let firstName = $('#firstNameInput').val();
     let lastName = $('#lastNameInput').val();
     let idTextInput = $('#idTextInput').val();
     let titleInput = $('#titleInput').val();
-    let annualSalary = $('#annualSalaryInput').val();
-    let submisionButton = $('#submitButton').val();
+    let annualSalary = parseFloat($('#annualSalaryInput').val());// using parseFloat to conver the input values to a decimal number. 
+
     // console.log('add employee button works');
     // console.log(tableRow)
-    let monthlyCost = annualSalary / 12;
 
+// using .append to add new employee data to our table. 
+// from our stored data above we can use string interpolation to add input values. 
     let tableRow = `<tr> <td>${firstName}</td><td>${lastName}</td><td>${idTextInput}</td><td>${titleInput}</td><td>${annualSalary}</td></tr></tbody>`
     // console.log(tableRow)
     $('#insertionPoint').append(tableRow);
 
-
+    $('.remove').last().on('click' deleteEmployees);
 
 }
+
+
+
+// X- A 'Submit' button should collect the form information------DONE
+// X -store the information append information to the DOM -------DONE
+// calculate monthly TOTAL, -------------------------------------TODO
+
+// clear the input fields. ---------------------------------------TODO
+// Using the stored information, calculate monthly costs ---------TODO
+// append this to the to DOM. ------------------------------------TODO
+
+// If the total monthly cost exceeds $20,000, 
+// add a red background color to the total monthly cost.-----------TODO
+
+// Create a delete button that removes an employee from the DOM.---TODO
+// For Base mode, it does **not** need to remove that Employee's salary 
+//from the reported total.-----------------------------------------TODO
