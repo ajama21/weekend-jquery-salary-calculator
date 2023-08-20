343722f550f32ae10d610ccf282a583e59060490
Abdirahman Jama <mustalid21@gmail.com>
2023-08-20 16:08:33 -0500

    added updateTotalMonthly function to calculate total monthly and styled the if statement to red >20000 monthly

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 $(document).ready(onReady);
-// console.log('js works!!!');
-let salaries =[];
-function onReady(){
-    // console.log('jquery is working!!!');
-    $('#submitButton').on('click', addEmployeeInfo)
+
+let salaries = []; // global array to store all salary data.
+
+function onReady() {
+    $('#submitButton').on('click', addEmployeeInfo);
 }
 
-
-//this function will be used to submit new employees info to the table. 
 function addEmployeeInfo(event) {
     event.preventDefault();
-// storing employee data from input using .val. 
     let firstName = $('#firstNameInput').val();
     let lastName = $('#lastNameInput').val();
     let idTextInput = $('#idTextInput').val();
     let titleInput = $('#titleInput').val();
-    let annualSalary = parseFloat($('#annualSalaryInput').val());// using parseFloat to conver the input values to a decimal number. 
+    let annualSalary = parseFloat($('#annualSalaryInput').val());
 
-    // console.log('add employee button works');
-    // console.log(tableRow)
+    let monthlySalary = annualSalary / 12;
+    salaries.push(monthlySalary); // Store monthly salary in the array
 
-// using .append to add new employee data to our table. 
-// from our stored data above we can use string interpolation to add input values. 
-    let tableRow = `<tr> <td>${firstName}</td><td>${lastName}</td><td>${idTextInput}</td><td>${titleInput}</td><td>${annualSalary}</td></tr></tbody>`
-    // console.log(tableRow)
+    let tableRow = `<tr><td>${firstName}</td><td>${lastName}</td><td>${idTextInput}</td><td>${titleInput}</td><td>${annualSalary}</td><td><button class="remove">Delete</button></td></tr>`;
     $('#insertionPoint').append(tableRow);
 
-    $('.remove').last().on('click', deleteEmployees);
+    $('.remove').last().on('click', deleteEmployee);
 
     updateTotalMonthly();
     
@@ -36,23 +30,46 @@ function addEmployeeInfo(event) {
     $('#idTextInput').val('');
     $('#titleInput').val('');
     $('#annualSalaryInput').val('');
-
-}
-function deleteEmployees (){
-
 }
 
+function updateTotalMonthly() {
+    let totalMonthly = 0;
 
-// X- A 'Submit' button should collect the form information------DONE
-// X -store the information append information to the DOM -------DONE
-// calculate monthly TOTAL, -------------------------------------TODO
+    // Calculate the total monthly salary using a for...of loop
+    for (let salary of salaries) {
+        totalMonthly += salary;
+    }
 
-// clear the input fields. ---------------------------------------TODO
-// Using the stored information, calculate monthly costs ---------TODO
-// append this to the to DOM. ------------------------------------TODO
+    $('#totalMonthlyCost').text(totalMonthly.toFixed(2));
 
-// If the total monthly cost exceeds $20,000, 
-// add a red background color to the total monthly cost.-----------TODO
+    if (totalMonthly > 20000) {
+        $('#totalMonthlyCost').addClass('high-total');
+    } else {
+        $('#totalMonthlyCost').removeClass('high-total');
+    }
+}
+
+function deleteEmployee() {
+
+}
+
+
+
+
+
+
+
+
+// X - A 'Submit' button should collect the form information------DONE
+// X - store the information append information to the DOM -------DONE
+// X - calculate monthly TOTAL, ----------------------------------DONE
+
+// X - clear the input fields. -----------------------------------DONE
+// X - Using the stored information, calculate monthly costs------DONE
+// X - Append this to the to DOM. --------------------------------TODO
+
+// X - If the total monthly cost exceeds $20,000, 
+// X - add a red background color to the total monthly cost.------TODO
 
 // X- Create a delete button that removes an employee from the DOM.---TODO
 // For Base mode, it does **not** need to remove that Employee's salary 
